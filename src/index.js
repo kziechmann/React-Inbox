2022d940642eddab7f9e3938bc85bca0af378dff
kziechmann <kziechmann@gmail.com>
2019-01-14 15:37:06 -0700

    Initialized project via create-react-app installed Bootstrap, Font-awesome and made messages, message and toolbar components.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
 import './index.css';
 import App from './App';
+import Messages from './Messages';
+import Toolbar from './Toolbar';
 import * as serviceWorker from './serviceWorker';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
