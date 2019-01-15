8150ce93bbaab6e0babe4719c81621e7fd88db7f
kziechmann <kziechmann@gmail.com>
2019-01-15 13:29:51 -0700

    created methods within the main App class for checking, selecting, and starring individual messages and setState() for each.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import 'font-awesome/css/font-awesome.css';
 import './App.css';
 
 class App extends Component {
+  state = {}
+
   componentDidMount = async() => {
     const response = await fetch('http://localhost:8082/api/messages')
     const messages = await response.json()
@@ -14,11 +16,33 @@ class App extends Component {
     this.setState(newState)
   }
 
+  checked = (e) => {
+    let newMessages = [...this.state.messages]
+    newMessages[e.target.id-1]['selected'] = e.target.checked
+    this.setState({messages: newMessages})
+  }
+
+  starred = (e) => {
+    let newMessages = [...this.state.messages]
+    let isStarred = newMessages[e.target.id-1]['starred'] || false
+    isStarred? newMessages[e.target.id-1]['starred'] = false : newMessages[e.target.id-1]['starred'] = true
+    this.setState({messages: newMessages})
+  }
+
+  read = (e) => {
+    let newMessages = [...this.state.messages]
+    newMessages[e.target.id-1]['read'] = true
+    this.setState({messages: newMessages})
+  }
+
   render() {
     return (
-      <div className="container">
-        <Toolbar/>
-        <Messages messages={this.state? this.state.messages :""}/>
+      <div>
+      <nav></nav>
+      <div className="container" style={{background: "rgba(200, 200, 200, 0.8)"}}>
+        <Toolbar messages={this.state.messages}/>
+        <Messages messages={this.state.messages} checked={this.checked} read={this.read} starred={this.starred}/>
+      </div>
       </div>
     );
   }
--- src/component/Message.js
+++ src/component/Message.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
 
 class Message extends Component {
+  state = { expanded: false }
+
+  expand = (e) =>{
+    e.preventDefault()
+    this.props.read(e)
+    this.setState({expanded: (this.state.expanded ? false : true)})
+  }
+
   render() {
     return (
-      <div class="row message unread">
-        <div class="col-xs-1">
-          <div class="row">
-            <div class="col-xs-2">
-              <input type="checkbox" />
+      <React.Fragment>
+      <div className={this.props.message.selected? (this.props.message.read? "row message read selected" : "row message unread selected" ): (this.props.message.read? "row message read unselected" : "row message unread unselected")}>
+        <div className="col-xs-1">
+          <div className="row">
+            <div className="col-xs-2">
+              <input type="checkbox" onClick={this.props.checked} id={this.props.message.id}/>
             </div>
-            <div class="col-xs-2">
-              <i class="star fa fa-star-o"></i>
+            <div className="col-xs-2">
+              <i className={this.props.message.starred? "star fa fa-star" : "star fa fa-star-o"} onClick={this.props.starred} id={this.props.message.id}></i>
             </div>
           </div>
         </div>
-        <div class="col-xs-11">
-          <a href="#">
-            Here is some message text that has a bunch of stuff
-          </a>
+        <div className="col-xs-11" >
+          {this.props.message.labels.includes('dev')? <span className="label label-warning">dev</span> : ""}
+          {this.props.message.labels.includes('personal')? <span className="label label-warning">gschool</span> : ""}
+          <small onClick={this.expand} id={this.props.message.id}>
+            {this.props.message.subject}
+          </small>
         </div>
       </div>
+        {this.state.expanded?
+        <div className="row message-body">
+          <div className="col-xs-11 col-xs-offset-1">
+            {this.props.message.body}
+          </div>
+        </div>
+        :""}
+      </React.Fragment>
     );
   }
 }
